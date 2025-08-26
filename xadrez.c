af0297b8700a126563f54ad8e8ce80a30d0b7655
guslel <gustavofagundselel@gmail.com>
2025-08-25 21:18:41 -0300

    desafio mestre

--- xadrez.c
+++ xadrez.c
@@ -9,75 +9,78 @@
 
   Informações do desafio Aventureiro:
     .Cavalo: Move-se em L, utilizando loop aninhado mova o cavalo 2 casas para baixo e 1 para esquerda.
+  
+  Informações do desafio Mestre:
+    .Torre, Bispo e Rainha: funções recursivas para simular o movimento de cada peça, substituindo os loops originais.
+    .Cavalo: loops aninhados com múltiplas variáveis e/ou condições para simular o movimento do Cavalo em "L" (duas casas para cima e uma para a direita).
+    .Bispo:Deve ser implementado com recursividade, e também com loops aninhados, utilizando o loop mais externo para o movimento vertical, e o mais interno 
+    para o movimento horizontal.
+
+  OBS: Trabalho atualizado conforme do desafio mestre!
 */
 
-int torre();
-int bispo();
-int rainha();
-int cavalo();
+//Movimento da torre usando função recursiva
+void torre(int i) {
+  if(i > 0) {
+    printf("-Cima\n");
+    torre(i-1);
+  }
+};
 
-int main() {
-  printf("### Movimentação de peças de xadrez ###\n");
-  torre();
-  bispo();
-  rainha();
-  cavalo();
+//Movimento do bispo usando função recursiva e aninhada
+void bispo(int i) {
+  printf("-Cima, ");
+  if(i > 0) {
+    printf("Direita\n");
+    if(i > 1) { //if implementado para não chamar mais 1 "-cima"
+      bispo(i-1);
+    }
+  }
+};
 
-  return 0;
+//Movimento da rainha usando função recursiva
+void rainha(int i) {
+  if(i > 0) {
+    printf("-Esquerda\n");
+    rainha(i-1);
+  }
+};
+
+//Movimento do cavalo usando função avançada e função aninhada
+void cavalo() {
+  int i, j;
+
+  for (i = 1; i <= 1;  i++) {
+    for (j = i; j <= 2 && i == 1 ; j+=(i)){
+      printf("-Cima\n");
+    }
+    
+    printf("-Direita\n");
+  }
 }
 
-int torre() {
-  int i = 1;
+/*
+  Função principal, passa a quantidade de execução para as
+  funções de movimento das peças
+*/
+int main() {
+  printf("### Movimentação de peças de xadrez ###\n");
 
   printf("  \n");
   printf("Movimento da torre: \n");
-  while (i <= 5){
-    printf("-Cima\n");
-    i++;
-  }
-
-  return 0;
-};
-
-int bispo() {
-  int i = 1;
+  torre(5);
 
   printf("  \n");
   printf("Movimento do bispo: \n");
-  do{
-    printf("-Cima, Direita\n");
-    i++;
-  } while (i <= 5);
-
-  return 0;
-};
-
-int rainha() {
-  int i = 1;
+  bispo(5);
 
   printf("  \n");
   printf("Movimento da Rainha: \n");
-
-  for (i; i <= 8;  i++) {
-    printf("-Esquerda\n");
-  }
-  
-  
-  return 0;
-};
-
-int cavalo() {
-  int i, j;
+  rainha(8);
 
   printf("  \n");
   printf("Movimento do Cavalo: \n");
+  cavalo();
 
-  for (i = 1; i <= 1;  i++) {
-    for (j = 1; j <= 2; j++){
-      printf("-Baixo\n");
-    }
-    
-    printf("-Esquerda\n");
-  }
   return 0;
 }
